4da556302829f17e7883095519373ec2560781c4
Dan Oswalt <do189705@gmail.com>
2016-06-18 13:28:32 -0700

    added db to add data from url params

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 var express = require('express'),
   port = process.env.PORT || 3000,
-  app = express();
+  app = express(),
+  sqlite3 = require('sqlite3').verbose(),
+  db = new sqlite3.Database('urlData.db');
+
+db.serialize(function() {
+  db.run('CREATE TABLE if not exists urlData (url TEXT)');
+  db.run('INSERT INTO urlData VALUES ("please work")');
+  db.run('INSERT INTO urlData (url) VALUES ("please work!!!")');
+  db.each('SELECT rowid AS id, url FROM urlData', function(err, row) {
+    console.log(row.id + ': ' + row.url);
+  });
+});
 
 app.use(express.static('./'));
 
 app.get('/', function(request, response) {
-  // console.log('New request:', request.url);
+  console.log('New request:', request.url);
   // response.sendFile('index.html', { root: '.' });
   response.send('<h1>I\'m a headline!</h1>');
 });
 
-app.get('/post/:bookmark', function(request, response){
+app.post('/post/:bookmark', function(request, response){
   var bookmark = request.params.bookmark;
-  //add to db
+  db.run('INSERT INTO urlData VALUES (?)', bookmark);
+  db.each('SELECT rowid AS id, url FROM urlData', function(err, row) {
+    console.log(row.id + ': ' + row.url);
+  });
+  console.log('add: ' + bookmark);
   response.send('<h1>' + bookmark + '</h1>');
-  console.log(bookmark);
 });
 
 app.listen(port, function() {
