46fd5da5e70419c1c596a3ee371b92693d1803d6
Dan Oswalt <do189705@gmail.com>
2016-06-18 16:31:29 -0700

    trying to add ajax call to bookmarkController

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ var express = require('express'),
   port = process.env.PORT || 3000,
   app = express(),
   sqlite3 = require('sqlite3').verbose(),
-  db = new sqlite3.Database('urlData.db');
+  db = new sqlite3.Database('urlData.db'),
+  urlData = [];
+
+//fetchfromDb is a 'middleware function' run whenever the page is loaded
+//it grabs all the info from the db
+var fetchFromDb = function(req, res, next) {
+
+  db.each('SELECT url FROM urlData', function(err, row) {
+    urlData.push(row.url);
+  }, next);
+
+};
+
+var sendData = function(request, response, data) {
+  response.send(data);
+};
 
 db.serialize(function() {
   db.run('CREATE TABLE if not exists urlData (url TEXT)');
@@ -11,13 +26,15 @@ db.serialize(function() {
   });
 });
 
-app.use(express.static('./'));
+// app.use(express.static('./'));
 
 app.get('/', function(request, response) {
+  // urlData = JSON.stringify(urlData);
   console.log('New request:', request.url);
   // response.sendFile('index.html', { root: '.' });
-  response.send('<h1>I\'m a headline!</h1>');
 });
+
+app.get('/data', fetchFromDb, sendData);
 
 app.get('/post/:bookmark', function(request, response){
   var bookmark = request.params.bookmark;
