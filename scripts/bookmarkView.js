edfb55632ea1935a9a99864eeaeddce3e8c42e8e
‘Aarman’ <aaron.beerman@gmail.com>
2016-06-18 14:03:28 -0700

    minor changes

---
+++ scripts/bookmarkView.js
@@ -0,0 +1,6 @@
+(function(module) {
+  var bookmark = [];
+  
+
+  module.bookmarkView = bookmarkView;
+})(window);
--- scripts/routes.js
+++ scripts/routes.js
@@ -1,27 +1,2 @@
-page('/post/:user/:bookmark', postUserBookmark, refreshPage);
 page('/', bookmarkController.index);
 page('/everyBkm', everyBkmController.index);
-/*
-  www.nabit.herokuapp.com/post/admin/www.nytimes.com
-
-  context.params.user = 'admin'
-  context.params.bookmark = 'www.nytimes.com'
-
-  function postUserBookmark(context, next) {
-    var stmt = db.prepare('INSERT INTO url_table VALUES (?)');
-
-    stmt.run(context.params.bookmark);
-
-    db.finalize();
-  }
-
-  bookmarklet:
-
-  javascript:(function(){
-    var user = 'admin'
-    $.ajax({
-      type: 'POST'
-      url: 'www.nabit.herokuapp.com/post/' + user + '/' + window.location.href
-    })
-  })();
-*/
