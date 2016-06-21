de04a2637b28229326979a56717bc3fc634bebd4
Dan Oswalt <do189705@gmail.com>
2016-06-20 17:09:31 -0700

    html not loading assets??

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
-page('/', bookmarkController.index);
-page('/everyBkm', everyBkmController.index);
-page();
-=======
 page('/', bookmarkController.index, function(){
   console.log(bookmarkController.urlData);
   console.log(context.params.urlData);
 });
->>>>>>> 790b4473f4182267b2a0767e4fb73cb0d73da0f4
+
+page('/users/:user', function() {
+  console.log('render it!');
+  console.log(context.userBookmarks);
+});
