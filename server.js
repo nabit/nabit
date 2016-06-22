238163a960afa3cf87effa0afe9edb821d604f19
Dan Oswalt <do189705@gmail.com>
2016-06-21 18:48:32 -0700

    made a REST api structure

--- server.js
+++ server.js
@@ -3,16 +3,28 @@ var express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
   sqlite3 = require('sqlite3').verbose(),
-  db = new sqlite3.Database('urlData.db'),
-  urlData = [];
+  db = new sqlite3.Database('urlData.db');
+
+var user = {
+  id : null,
+  name : null,
+  bookmarks : null
+};
+
+function Bookmark(title, url, timestamp) {
+  this.title = title;
+  this.url = url;
+  this.timestamp = timestamp;
+}
 
 //_______serialize db____________________//
 
 db.serialize(function() {
-  db.run('CREATE TABLE if not exists urlData (url VARCHAR(150), title VARCHAR(150))');
-  db.each('SELECT rowid AS id, url, title FROM urlData', function(err, row) {
-    // console.log(row.id + ': ' + row.url + ' ' + title);
-  });
+  db.run('CREATE TABLE if not exists users (username VARCHAR(150), password VARCHAR(150))');
+  db.run('CREATE TABLE if not exists bookmarks (user_id INT url VARCHAR(150), title VARCHAR(150), timestamp REAL)');
+  // db.each('SELECT rowid AS id, url, title FROM urlData', function(err, row) {
+  //   console.log(row.id + ': ' + row.url + ' ' + title);
+  // });
 });
 
 //_______initialize routes____________________//
@@ -21,13 +33,105 @@ app.use(express.static('./'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+//_______REST routes____________________//
+
+//is this needed??
 app.get('/', function(request, response) {
   console.log('New req:', request.url);
   response.sendFile('index.html', { root: '.' });
 });
 
-//when a user is visits this endpoint, fetch their bookmarks from DB
-//for now, there are no users, so this will fetch a list of urls
+//GET user
+app.get('/:user',
+
+  function(request, response, next){
+    //db statement
+  },
+  function(request, response) {
+
+    //send something
+    response.send();
+  }
+);
+
+//POST user
+app.post('/:user',
+
+  function(request, response, next){
+    //db statement
+  },
+  function(request, response) {
+
+    //send something
+    response.send();
+  }
+);
+
+//DELETE user by username
+app.delete('/:user',
+
+  function(request, response, next){
+    //db statement
+  },
+  function(request, response) {
+
+    //send something
+    response.send();
+  }
+);
+
+//GET all user bookmarks
+app.get('/:user/bookmarks',
+
+  function(request, response, next){
+    //db statement
+  },
+  function(request, response) {
+
+    //send something
+    response.send();
+  }
+);
+
+//GET user bookmark by id
+app.get('/:user/bookmarks/:id',
+
+  function(request, response, next){
+    //db statement
+  },
+  function(request, response) {
+
+    //send something
+    response.send();
+  }
+);
+
+//POST user bookmark
+app.get('/:user/bookmarks/bookmark',
+
+  function(request, response, next){
+    //db statement
+  },
+  function(request, response) {
+
+    //send something
+    response.send();
+  }
+);
+
+//DELETE user bookmark by id
+app.delete('/:user/bookmarks/:id',
+
+  function(request, response, next){
+    //db statement
+  },
+  function(request, response) {
+
+    //send something
+    response.send();
+  }
+);
+
 app.get('/users/:username',
 
   function(request, response, next) {
@@ -66,6 +170,8 @@ app.post('/users/user/bookmark', function(request, response){
   console.log('Title= ' + title + 'and url=' + url);
   response.send({title : title, url : url});
 });
+
+//_______listen up!____________________//
 
 app.listen(port, function() {
   console.log('Server started on port ' + port + '!' );
